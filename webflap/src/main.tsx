b7f82de78f8ce08876ff6ac2f9b0194e5da20974
Janny Nguyen <janvyngu@gmail.com>
2025-11-03 09:21:01 -0600

    Added in basic home/landing page

--- webflap/src/main.tsx
+++ webflap/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Home from './Home.tsx'
 import Grammars from './Grammars.tsx'
 import BruteForceParse from './BruteForceParse'
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/grammars" element={<Grammars />} />
         <Route path="/bruteforce" element={<BruteForceParse />} />
       </Routes>
