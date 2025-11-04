92775b06a9aa5f45c30e641f657986d1b4c20661
Jossen Rinn <jossenrinn3@gmail.com>
2025-11-04 17:35:27 -0600

    Add Features route and component

--- webflap/src/main.tsx
+++ webflap/src/main.tsx
@@ -1,3 +1,33 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import './index.css'
+// import App from './App.tsx'
+// import Home from './Home.tsx'
+// import Grammars from './Grammars.tsx'
+// import BruteForceParse from './BruteForceParse'
+// import Automatons from './Automatons.tsx'
+// import ProjectSelection from './ProjectSelection.tsx'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/grammars" element={<Grammars />} />
+//         <Route path="/bruteforce" element={<BruteForceParse />} />
+//         <Route path="/automatons" element={<Automatons />} />
+//         <Route path="/new" element={<ProjectSelection />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -8,6 +38,7 @@ import Grammars from './Grammars.tsx'
 import BruteForceParse from './BruteForceParse'
 import Automatons from './Automatons.tsx'
 import ProjectSelection from './ProjectSelection.tsx'
+import Features from './Features.tsx' // ✅ added Features page
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -19,6 +50,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/bruteforce" element={<BruteForceParse />} />
         <Route path="/automatons" element={<Automatons />} />
         <Route path="/new" element={<ProjectSelection />} />
+        <Route path="/features" element={<Features />} /> {/* ✅ new route */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
