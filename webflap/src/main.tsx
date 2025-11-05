3cab5688c19b19d84ef612949065f51080b54461
callumscottward <callumward@unomaha.edu>
2025-11-05 13:16:44 -0600

    Rename automatons to automata and replace App with Home

--- webflap/src/main.tsx
+++ webflap/src/main.tsx
@@ -1,57 +1,25 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import './index.css'
-// import App from './App.tsx'
-// import Home from './Home.tsx'
-// import Grammars from './Grammars.tsx'
-// import BruteForceParse from './BruteForceParse'
-// import Automatons from './Automatons.tsx'
-// import ProjectSelection from './ProjectSelection.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./Home.tsx";
+import Grammars from "./Grammars.tsx";
+import BruteForceParse from "./BruteForceParse";
+import Automata from "./Automata.tsx";
+import ProjectSelection from "./ProjectSelection.tsx";
+import Features from "./Features.tsx"; // ✅ added Features page
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<App />} />
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/grammars" element={<Grammars />} />
-//         <Route path="/bruteforce" element={<BruteForceParse />} />
-//         <Route path="/automatons" element={<Automatons />} />
-//         <Route path="/new" element={<ProjectSelection />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
-
-
-
-
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-import Home from './Home.tsx'
-import Grammars from './Grammars.tsx'
-import BruteForceParse from './BruteForceParse'
-import Automatons from './Automatons.tsx'
-import ProjectSelection from './ProjectSelection.tsx'
-import Features from './Features.tsx' // ✅ added Features page
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/grammars" element={<Grammars />} />
         <Route path="/bruteforce" element={<BruteForceParse />} />
-        <Route path="/automatons" element={<Automatons />} />
+        <Route path="/automata" element={<Automata />} />
         <Route path="/new" element={<ProjectSelection />} />
-        <Route path="/features" element={<Features />} /> {/* ✅ new route */}
+        <Route path="/features" element={<Features />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
