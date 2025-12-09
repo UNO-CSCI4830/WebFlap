77191f61d213209c3bed43e16a56d5e09c0fc3f6
Reid Chapman <rmchapman2004@gmail.com>
2025-12-09 10:29:13 -0600

    created regex page and connected its route

---
+++ webflap/src/Regex.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect, useRef } from 'react';
+import NavigationBar from "./NavigationBar";
+
+function Regex() {
+
+
+    return(
+        <div className="jflapContainer">
+            <NavigationBar />
+        </div>
+    )
+
+}
+export default Regex
